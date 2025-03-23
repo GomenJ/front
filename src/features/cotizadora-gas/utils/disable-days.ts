4c5e9f285f60717fb9652f2234f3a0362fc8efea
GomenJ <juan.gonzalez.mendoza.j@gmail.com>
2025-03-22 22:05:08 -0600

    fix: fix date typo

--- src/features/cotizadora-gas/utils/disable-days.ts
+++ src/features/cotizadora-gas/utils/disable-days.ts
@@ -7,21 +7,17 @@ export const disableDays = (
 	user: Omit<User, "password"> | null,
 ) => {
 	// const todayDate = format(new Date(), "yyyy-MM-dd");
-	const date = format(day.toLocaleDateString(), "yyyy-MM-dd");
+	const date = format(day, "yyyy-MM-dd");
 
 	if (user?.role === "admin") {
 		return !availableDates.includes(date);
 	}
 
-	if (availableDates.length === 0 && !availableDates) return false;
-	// return !availableDates.includes(todayDate) && date === todayDate;
-
 	if (availableDates[0] === date) {
 		return false;
 	}
-	// if (availableDates.includes(date) && date === todayDate) {
-	// 	return false;
-	// }
+
+	// return !availableDates.includes(todayDate) && date === todayDate;
 
 	return true;
 };
